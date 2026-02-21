eff04d9d66a93427d686691b8637333c3ac683da
ddevourV <recklessfoxl33t@gmail.com>
2026-02-21 04:02:08 +0300

    Update Settings.h

--- include/Settings.h
+++ include/Settings.h
@@ -2,6 +2,16 @@
 
 namespace Settings
 {
+    inline bool enabled = true;
+
+    // базовые дефолты для мувмента
+    inline float strafeDeadzone = 0.10f;
+    inline float minStrafeAngle = 5.0f;
+
+    // чтобы другие файлы не падали, если где-то упоминается
+    inline bool enableFovZoom = false;
+    inline bool enableTremble = false;
+
     inline bool LoadSettings()
     {
         return true;
